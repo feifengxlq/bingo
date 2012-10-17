8332733e4f8d51af3c5ad2e6c9a0ab2677b38a5e
feifengxlq <feifengxlq@gmail.com>
2012-10-17 13:55:15 +0800

    add Bingo

---
+++ libs/Bingo.php
@@ -0,0 +1,24 @@
+<?php
+/***************************************************************************
+ * 
+ * Copyright (c) 2012 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file libs/Bingo.php
+ * @author xuliqiang(com@baidu.com)
+ * @date 2012/10/17 13:54:32
+ * @brief 
+ *  
+ **/
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
+?>
---
+++ test/bingo.php
@@ -0,0 +1,24 @@
+<?php
+/***************************************************************************
+ * 
+ * Copyright (c) 2012 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file test/bingo.php
+ * @author xuliqiang(com@baidu.com)
+ * @date 2012/10/17 13:54:50
+ * @brief 
+ *  
+ **/
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
+?>
